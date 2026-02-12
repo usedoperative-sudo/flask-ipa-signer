e833ffbf70e6352648e4c146094320ad2a6d0c97
joshuajohann <valletejerojoshuajohann@gmail.com>
2026-02-12 13:08:09 -0600

    Update setup script for zsign installation
    Install necessary packages and clone zsign repository.

--- setup.sh
+++ setup.sh
@@ -2,12 +2,19 @@ sudo apt install -y g++ pkg-config libssl-dev libminizip-dev git make
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 brew install cloudflared
+cd $HOME
 git clone https://github.com/zhlynn/zsign.git
 cd zsign/build/linux
 make clean && make
 cd $HOME/zsign/bin
+sudo mv /home/linuxbrew/.linuxbrew/bin/cloudflared $PREFIX/bin/cloudflared
 sudo mv zsign $PREFIX/usr/bin
 sudo chmod +x $PREFIX/usr/bin/zsign
 sudo pip install Flask
+sudo apt install python3-flask
 clear
 echo "Preparation done!"
+echo "SHOWING INSTALLED COMMANDS:"
+command -v zsign
+command -v cloudflared
+apt search python3-flask
