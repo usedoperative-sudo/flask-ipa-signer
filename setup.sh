ba27dbef6fcfca039cace160fcad5663099199ff
usedoperative-sudo <usedoperative@gmail.com>
2026-02-15 00:08:51 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,11 +10,10 @@ cd $HOME/zsign/bin
 sudo mv /home/linuxbrew/.linuxbrew/opt/cloudflared/bin/cloudflared $PREFIX/bin/cloudflared
 sudo mv zsign $PREFIX/usr/bin
 sudo chmod +x $PREFIX/usr/bin/zsign
-sudo pip install Flask
 sudo apt install python3-flask
 clear
 echo "Preparation done!"
 echo "SHOWING INSTALLED COMMANDS:"
 command -v zsign
 command -v cloudflared
-apt search python3-flask
+echo "Python lib - python3-flask"
