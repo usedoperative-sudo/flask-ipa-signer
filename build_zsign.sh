5bef4ec86b54701dddc1ef9f06e02eebec3c8bef
Used <usedoperative@gmail.com>
2026-04-04 20:14:04 -0600

    Refactor architecture detection in build_zsign.sh

--- build_zsign.sh
+++ build_zsign.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# 🔍 Detectar la arquitectura dinámicamente (x86_64, aarch64, etc.)
-MULTIARCH=$(gcc -print-multiarch)
-
-echo "🔧 Creating minizip-ng shim for architecture: $MULTIARCH"
-
+echo "🔧 Creating minizip-ng shim for architecture: $(gcc -print-multiarch)"
 sudo mkdir -p /usr/local/lib/pkgconfig
-sudo bash -c "cat << EOF > /usr/local/lib/pkgconfig/minizip-ng.pc
+sudo bash -c 'cat << EOF > /usr/local/lib/pkgconfig/minizip-ng.pc
 prefix=/usr
 exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib/$MULTIARCH
+libdir=\${exec_prefix}/lib/$(uname -m)-linux-gnu
 includedir=\${prefix}/include/minizip
 
 Name: minizip-ng
@@ -18,7 +14,9 @@ Description: Minizip-ng shim for zsign
 Version: 3.0.0
 Libs: -L\${libdir} -lminizip
 Cflags: -I\${includedir}
-EOF"
+EOF'
+sleep 1s
+echo "🔹 Done!"
 
 echo "📥 Cloning and building zsign..."
 git clone https://github.com/zhlynn/zsign.git
